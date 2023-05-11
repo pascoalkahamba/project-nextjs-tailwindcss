5d4673cd2863bffd926b607540d0daf118abc004
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-05-11 18:15:59 +0100

    I've added the functionality to change the theme for the dark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,30 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/global.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
+  const router = useRouter();
 
+  useEffect(() => {
+    const handleRouteChange = () => {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    };
+
+    handleRouteChange();
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Head>
@@ -15,6 +35,7 @@ export default function App(props: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
+
       <Component {...pageProps} />
     </>
   );
