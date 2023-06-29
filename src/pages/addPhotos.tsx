5fb2bd0b6d334a9faa6e0083dae9ecc4380bc222
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-06-29 20:21:34 +0100

    I've created the component addPhotos

---
+++ src/pages/addPhotos.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const AddPhotos = () => {
+  return (
+    <Layout>
+      <div>add your favorite photo over here!</div>
+    </Layout>
+  );
+};
+
+export default AddPhotos;
